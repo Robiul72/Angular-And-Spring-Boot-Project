7a32b9186cebce3f169f19da76fc232aee66b73d
Robiul72 <robiulislam366972@gmail.com>
2024-01-28 20:26:25 +0600

    add
    28-01-2024

---
+++ HouseRent/src/main/java/com/houserent/HouseRent/model/HouseRentModel.java
@@ -0,0 +1,77 @@
+package com.houserent.HouseRent.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity(name = "house_list")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class HouseRentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String build_Name;
+    private String owner_Name;
+    private String floor_No;
+    private String unit_No;
+    private String bedroom;
+    private String location;
+    private String description;
+
+    private String monthly_rent;
+    private String electricity_rent;
+    private String water_rent;
+    private String garbage_rent;
+    private String total_cost;
+    private String total_Advance;
+    private String total_Due;
+    private String due_Pay;
+
+    private String image;
+
+    @Temporal(TemporalType.TIMESTAMP)
+//    @Column(name = "date", nullable = false, updatable = false)
+    private Date date;
+
+    @PrePersist
+    protected void onCreate() {
+        date = new Date();
+    }
+
+
+
+    @ManyToOne
+    @JoinColumn(name="owner_id")
+    OwnerModel ownerName;
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//    private String h_Rent_Area;
+//    private String h_Rent_Owner_Name;
+//
+//    private String h_Rent_Name;
+//    private String h_Rent_Category;
+//    private String h_Rent_Bedroom;
+//    private String h_Rent_Price;
+//
+//    @Column(unique = true, nullable = false)
+//    private String h_Rent_Email;
+//
+//    private String h_Rent_Mobile;
+//    private String h_Rent_Address;
+//    private String h_Rent_Description;
+
+//    private String h_Rent_Image;
+
+
+}
---
+++ HouseRent/src/main/java/com/houserent/HouseRent/model/OwnerModel.java
@@ -0,0 +1,37 @@
+package com.houserent.HouseRent.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "owner_list")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class OwnerModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String h_Rent_NID;
+    private String h_Rent_Owner_Name;
+
+    private String h_Rent_Date;
+
+    @Column(unique = true, nullable = false)
+    private String h_Rent_Email;
+
+    private String h_Rent_Mobile;
+
+    private String image;
+
+    private String h_Date;
+
+
+
+
+
+}
